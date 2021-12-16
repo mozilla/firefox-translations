e63a3517415b859df65ee2131ab4437bcc61d3c5
Andre Natal <anatal@gmail.com>
2021-12-16 02:25:13 -0800

    Updating bergamot translator to commit 420f1sb

--- extension/model/engineRegistry.js
+++ extension/model/engineRegistry.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
 
-const engineRegistryRootURL = "https://101-346428477-gh.circle-artifacts.com/0/wasm-without-wormhole/";
+const engineRegistryRootURL = "https://108-346428477-gh.circle-artifacts.com/0/wasm-without-wormhole/";
 
 const engineRegistry = {
     bergamotTranslatorWasm: {
         fileName: "bergamot-translator-worker.wasm",
-        fileSize: 6876025,
-        sha256: "b44bb14a4fe3b3d9c347d18825bf4c2a5b3ee90c3796e45b660f4ee11ad17531"
+        fileSize: 6875246,
+        sha256: "c2f5ad88723b6b34401c170c379e51911fc64d33349ac20ee4379e6a45361a1d"
     }
 }
