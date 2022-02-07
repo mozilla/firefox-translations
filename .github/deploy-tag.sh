1c5db0c8cb4c623efb2d690982d6a2bf0cb4b570
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2022-02-07 16:50:44 +0000

    Allow long refs, fix regex

--- .github/deploy-tag.sh
+++ .github/deploy-tag.sh
@@ -2,6 +2,6 @@
 cd extension
 mv manifest.json{,.bak}
 < manifest.json.bak \
-sed 's/"version": ".+"/"version": "'$(echo "$GITHUB_REF_NAME" | sed -r 's/v(.+)$/\1/')'"/' \
+sed -r 's/"version": ".+"/"version": "'$(echo "$GITHUB_REF_NAME" | sed -r 's/^(.+\/)?v(.+)$/\2/')'"/' \
 | sed 's/"version_name": ".+"/"version_name": "'"$GITHUB_SHA"'"/' \
 > manifest.json
