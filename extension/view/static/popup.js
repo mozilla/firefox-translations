8ad72e113608c7d3b7392c0d23f33fef0e63b6ca
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2022-02-16 16:45:04 +0000

    Simple generic binding schema in HTML
    Because I'm not in the mood to write Javascript code for each element

--- extension/view/static/popup.js
+++ extension/view/static/popup.js
@@ -25,34 +25,48 @@ function renderSelect(select, values) {
 	Object.entries(values).forEach(([value, label]) => select.add(new Option(label, value), null));
 }
 
-function render(state) {
-	console.log('[popup] Render', state);
+function queryXPathAll(query, callback) {
+	const result = document.evaluate(query, this instanceof Element ? this : document.body, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+	let output = [];
+	let element;
+	while (element = result.iterateNext()) {
+		output.push(element);
+	};
+	return output;
+}
 
+function render(state) {
+	const regionNamesInEnglish = new Intl.DisplayNames(['en'], {type: 'language'});
+
+	state = {
+		...state,
+		'lang-from': regionNamesInEnglish.of(state.from),
+		'lang-to': regionNamesInEnglish.of(state.to),
+		'lang-from-options': Object.fromEntries(state.models.map(({from}) => [from, regionNamesInEnglish.of(from)])),
+		'lang-to-options': Object.fromEntries(state.models.map(({to}) => [to, regionNamesInEnglish.of(to)])),
+		'completedTranslationRequests': state.totalTranslationRequests - state.pendingTranslationRequests
+	};
+
+	// Toggle "hidden" state of all <div data-state=""> elements
 	document.querySelectorAll('*[data-state]').forEach(el => {
 		el.hidden = el.dataset.state != state.state;
 	});
 
-	const regionNamesInEnglish = new Intl.DisplayNames(['en'], {type: 'language'});
+	// Assign values to each element that has <div data-bind:something=""> attributes
+	queryXPathAll('//*[@*[starts-with(name(), "data-bind:")]]').forEach(el => {
+		Object.entries(el.dataset).forEach(([key, value]) => {
+			let match = key.match(/^bind:(.+)$/);
+			if (!match) return;
 
-	const fromOptions = Object.fromEntries(state.models
-		.map(({from}) => [from, regionNamesInEnglish.of(from)]));
-	renderSelect(document.querySelector('#lang-from'), fromOptions);
-	document.querySelector('#lang-from').value = state.from;
-
-	const toOptions = Object.fromEntries(state.models
-		.map(({to}) => [to, regionNamesInEnglish.of(to)]));
-	renderSelect(document.querySelector('#lang-to'), toOptions);
-	document.querySelector('#lang-to').value = state.to;
-
-	const progress = state.totalTranslationRequests - state.pendingTranslationRequests;
-	if (progress)
-		document.querySelector('#progress-bar').value = progress;
-	else
-		document.querySelector('#progress-bar').removeAttribute('value'); // gives you a nice I DONT KNOW?! kind of style progress bar during model loading ;)
-	document.querySelector('#progress-bar').max = state.totalTranslationRequests;
-
-	document.querySelectorAll('input[type=checkbox][data-state-key]').forEach(el => {
-		el.checked = state[el.dataset.stateKey];
+			switch (match[1]) {
+				case 'options':
+					renderSelect(el, state[value]);
+					break;
+				default:
+					el[match[1]] = state[value];
+					break;
+			}
+		});
 	});
 }
 
@@ -98,11 +112,11 @@ browser.tabs.query({active: true, currentWindow: true}).then(tabs => {
 				command: 'TranslateAbort'
 			});
 		},
-		'change input[type=checkbox][data-state-key]': e => {
+		'change input[type=checkbox][data-bind\\:checked]': e => {
 			backgroundScript.postMessage({
 				command: 'UpdateRequest',
 				data: {
-					[e.target.dataset.stateKey]: e.target.checked
+					[e.target.dataset['bind:checked']]: e.target.checked
 				}
 			})
 		}
