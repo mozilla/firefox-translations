525b22aafa3dd36fc8b2595b2b42fe5b7c5dc0bc
Abhishek Aggarwal <aaggarwal@mozilla.com>
2022-02-18 13:22:57 +0100

    Using v0.4.1 artifacts for the engine

--- extension/model/engineRegistry.js
+++ extension/model/engineRegistry.js
@@ -1,13 +1,12 @@
 /* eslint-disable no-unused-vars */
 
-//let engineRegistryRootURL = "https://github.com/mozilla/bergamot-translator/releases/download/0.3.1%2B793d132/";
-let engineRegistryRootURL = "http://localhost/js/";
+let engineRegistryRootURL = "https://github.com/mozilla/bergamot-translator/releases/download/v0.4.1/";
 const engineRegistryRootURLTest = "https://example.com/browser/browser/extensions/translations/test/browser/";
 
 const engineRegistry = {
     bergamotTranslatorWasm: {
-        fileName: "bergamot-translator-worker.wasm",
-        fileSize: 6922513,
-        sha256: "32dce15a1e444ca677808485186648f5f3657ddde2ddc4349443655b37296c3f"
+        fileName: "bergamot-translator-worker-with-wormhole.wasm",
+        fileSize: 6923132,
+        sha256: "26c24d3695eab02e91f20c68a8c1f7587d2a3e338a6f057f407c39a7e6c2c321"
     }
 }
