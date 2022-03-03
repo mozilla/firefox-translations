c51083cf807bb819db6cbe164234c7f403311e16
Abhishek Aggarwal <aaggarwal@mozilla.com>
2022-03-03 13:03:40 +0100

    Updated bergamot engine to 0.4.2

--- extension/model/engineRegistry.js
+++ extension/model/engineRegistry.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-unused-vars */
 
-let engineRegistryRootURL = "https://github.com/mozilla/bergamot-translator/releases/download/v0.4.1/";
+let engineRegistryRootURL = "https://github.com/mozilla/bergamot-translator/releases/download/v0.4.2/";
 const engineRegistryRootURLTest = "https://example.com/browser/browser/extensions/translations/test/browser/";
 
 const engineRegistry = {
     bergamotTranslatorWasm: {
         fileName: "bergamot-translator-worker-with-wormhole.wasm",
-        fileSize: 6923132,
-        sha256: "26c24d3695eab02e91f20c68a8c1f7587d2a3e338a6f057f407c39a7e6c2c321"
+        fileSize: 6929232,
+        sha256: "cccc905f2ccbdbf13d38e640151f06f04ed5568f34fd1ee1a6c4bf73147e2c3f"
     }
 }
