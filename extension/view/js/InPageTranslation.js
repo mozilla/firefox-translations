7431343a69ee719865fdcb07dd292562eb8d5313
Abhishek Aggarwal <66322306+abhi-agg@users.noreply.github.com>
2022-05-03 17:50:51 +0200

    Replaces unsafe use of innerHTML and appendChild (#281)
    * Replaces unsafe use of innerHTML and appendChild
     - Sanitize the qe specific html text that is added via appendChild
     - Using DocumentFragment to update dom instead of setting innerHTML directly
    * Addressed reviewer's comments
     - Moved hasOnlyQEAttributes outside the loop
     - Use children instead of childNodes to check for QE attributes of
       a node's children
     - Case insensitive search for the font tag

--- extension/view/js/InPageTranslation.js
+++ extension/view/js/InPageTranslation.js
@@ -27,6 +27,10 @@ class InPageTranslation {
         this.withOutboundTranslation = null;
         this.withQualityEstimation = null;
         this.QE_THRESHOLD = Math.log(0.5);
+        this.qeAttributes = new Set([
+            "x-bergamot-sentence-index", "x-bergamot-sentence-score",
+            "x-bergamot-word-index", "x-bergamot-word-score",
+        ]);
 
         /*
          * reference for all tags:
@@ -527,8 +531,13 @@ class InPageTranslation {
             // console.groupCollapsed(computePath(node));
             node.setAttribute("x-bergamot-translated", "");
 
-            const scratch = node.cloneNode(false); // shallow clone of parent node
-            scratch.innerHTML = translatedHTML;
+            /*
+             * create a scratch (a DocumentFragment) from translated html that will be
+             * used to populate the live tree with the translated content
+             */
+            const nonLiveDomContainer = document.createElement("template");
+            nonLiveDomContainer.innerHTML = translatedHTML;
+            const scratch = nonLiveDomContainer.content;
 
             const originalHTML = node.innerHTML;
 
@@ -553,6 +562,18 @@ class InPageTranslation {
                             break;
                     }
                 });
+            };
+
+            // check (recursively) if a given node and all its children have only QE specific attributes
+            const hasOnlyQEAttributes = node => {
+                if (node.nodeType === Node.ELEMENT_NODE) {
+                    if (node.nodeName.toUpperCase() !== "FONT") return false;
+
+                    if (!node.getAttributeNames().every(attribute => this.qeAttributes.has(attribute))) return false;
+
+                    for (let child of node.children) if (!hasOnlyQEAttributes(child)) return false;
+                }
+                return true;
             };
 
             /*
@@ -586,11 +607,11 @@ class InPageTranslation {
 
                             /*
                              * if translated element child doesn't have data-x-bergamot-id attribute and
-                             * has quality score specific attributes (that are set by translation engine
+                             * has only quality score specific attributes (that are set by translation engine
                              * when QE is on) then just add the translated element child to the live
                              * element node.
                              */
-                            if (!child.hasAttribute("data-x-bergamot-id") && (child.hasAttribute("x-bergamot-sentence-index") || child.hasAttribute("x-bergamot-word-index"))) {
+                            if (!child.hasAttribute("data-x-bergamot-id") && hasOnlyQEAttributes(child)) {
                                 dst.appendChild(child);
                             } else {
                                 console.warn(`[InPlaceTranslation] ${this.computePath(child, scratch)} Could not find counterpart for`, child.dataset.xBergamotId, dstChildNodes, child);
