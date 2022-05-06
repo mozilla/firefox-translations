e3b5c0e501e7f0d64d0976d25ff86a8dabf42274
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2022-05-06 16:02:30 +0100

    Improve translateLocally detection
    Better test: if it responds (with a valid message, or an error) it exists. If it disconnects, it doesn't. `onDisconnect` is not called when disconnecting manually. And even if it is, at that point the promise has already been resolved.

--- extension/view/static/options.js
+++ extension/view/static/options.js
@@ -1,38 +1,52 @@
 // Defaults. Duplicated in backgroundScript.js :(
 
-const state = {
+const globalState = {
 	provider: 'wasm',
-	translateLocallyAvailable: false,
 	developer: false,
+};
+
+const localState = {
+	translateLocallyAvailable: false,
 	get benchmarkURL() {
 		return compat.runtime.getURL('view/static/benchmark.html');
 	}
 };
 
-compat.storage.local.get().then(localState => {
-	Object.assign(state, localState);
-	renderBoundElements(document.body, state);
+const render = () => renderBoundElements(document.body, {...globalState, ...localState});
+
+compat.storage.local.get().then(state => {
+	Object.assign(globalState, state);
+	render();
 });
 
 compat.storage.onChanged.addListener(async changes => {
 	Object.entries(changes).forEach(([key, {newValue}]) => {
-		state[key] = newValue;
+		globalState[key] = newValue;
 	});
-	renderBoundElements(document.body, state);
+	render();
 });
 
 addBoundElementListeners(document.body, (key, value) => {
 	compat.storage.local.set({[key]: value});
 });
 
-const port = compat.runtime.connectNative('translatelocally');
-port.onDisconnect.addListener(e => {
-	console.log('onDisconnect', port.error);
-	if (port.error || compat.runtime.lastError) {
-		state.translateLocallyAvailable = false;
-		renderBoundElements(document.body, state);
-	}
+function canTranslateLocally() {
+	return new Promise((resolve, reject) => {
+		const port = compat.runtime.connectNative('translatelocally');
+		port.onDisconnect.addListener(() => resolve(false));
+		port.onMessage.addListener(message => {
+			resolve(true);
+			port.disconnect();
+		});
+		port.postMessage({
+			"id": 1,
+			"command": "Version",
+			"data": {}
+		});
+	});
+}
+
+canTranslateLocally().then(available => {
+	localState.translateLocallyAvailable = available;
+	render();
 });
-port.disconnect();
-state.translateLocallyAvailable = true;
-renderBoundElements(document.body, state);
