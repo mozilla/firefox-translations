43fde1e7909f93215afe9778f107cea9e785ba55
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2022-08-19 12:29:40 +0100

    Patch webpack to not make a mess of webworker urls

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,5 +1,47 @@
 import WebExtPlugin from 'web-ext-plugin';
 import CopyPlugin from 'copy-webpack-plugin';
+import webpack from 'webpack';
+
+/**
+ * Hack replacement of webpack's default baseURI runtimme implementation that
+ * returns a string or undefined. And that doesn't work well when used with
+ * `new URL(..., baseURI)` where the base URI is a secret plugin installation
+ * path only known at runtime.
+ */
+class PatchedBaseUriRuntimeModule extends webpack.RuntimeModule {
+  constructor() {
+    super("base uri", webpack.RuntimeModule.STAGE_ATTACH);
+  }
+
+  generate() {
+    return `${webpack.RuntimeGlobals.baseURI} = self.location;`;
+  }
+}
+
+class PatchedBaseUriPlugin {
+  apply(compiler) {
+    compiler.hooks.compilation.tap('RuntimePlugin', compilation => {
+      const globalChunkLoading = compilation.outputOptions.chunkLoading;
+      const isChunkLoadingDisabledForChunk = chunk => {
+        const options = chunk.getEntryOptions();
+        const chunkLoading =
+          options && options.chunkLoading !== undefined
+            ? options.chunkLoading
+            : globalChunkLoading;
+        return chunkLoading === false;
+      };
+
+      compilation.hooks.runtimeRequirementInTree
+        .for(webpack.RuntimeGlobals.baseURI)
+        .tap("RuntimePlugin", chunk => {
+          if (isChunkLoadingDisabledForChunk(chunk)) {
+            compilation.addRuntimeModule(chunk, new PatchedBaseUriRuntimeModule());
+            return true;
+          }
+        });
+    })
+  }
+}
 
 const module = {
   rules: [
@@ -18,10 +60,11 @@ export default {
     'background-script': './src/background/background-script.js',
     'benchmark': './src/benchmark/benchmark.js',
     'options': './src/options/options.js',
-    'popup': './src/popup/popup.js',
+    'popup': './src/popup/popup.js'
   },
   output: {
     path: new URL("./extension", import.meta.url).pathname,
+    publicPath: '',
     chunkFormat: 'array-push',
     assetModuleFilename: '[name][ext]',
     globalObject: 'self',
@@ -34,6 +77,7 @@ export default {
   },
   devtool: "source-map",
   plugins: [
+    new PatchedBaseUriPlugin(),
     new CopyPlugin({
       patterns: [
         {
