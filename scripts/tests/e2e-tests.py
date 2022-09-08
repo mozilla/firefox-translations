7e01c3087950ee58b18bae6854e9294b8eb2753d
Andre Natal <andrenatal@users.noreply.github.com>
2022-09-07 23:11:23 -0700

    Update e2e-tests.py

--- scripts/tests/e2e-tests.py
+++ scripts/tests/e2e-tests.py
@@ -78,10 +78,7 @@ with open('gecko/browser/extensions/translations/moz.build', 'a') as f:
 print("****** Test with faster gemm ******")
 try:
     print("Building gecko")
-    subprocess.check_output("./mach bootstrap", stderr=subprocess.STDOUT, shell=True, universal_newlines=True, cwd="gecko")
-    isExist = os.path.exists("/Users/runner/srcdirs/gecko-4825a4822c73/_virtualenvs/mach/lib/python3.10/site-packages/")
-    print(isExist)
-    subprocess.check_output("MOZBUILD_STATE_PATH=/Users/runner/ ./mach build", stderr=subprocess.STDOUT, shell=True, universal_newlines=True, cwd="gecko")
+    subprocess.check_output("MOZBUILD_STATE_PATH=/Users/runner/gecko/ ./mach build", stderr=subprocess.STDOUT, shell=True, universal_newlines=True, cwd="gecko")
     print("Running test with faster gemm")
     subprocess.check_output("./mach test --setpref=fxtranslations.running.mochitest=true browser/extensions/translations/test/browser/browser_translation_test.js", stderr=subprocess.STDOUT, shell=True, universal_newlines=True, cwd="gecko")
     print("Test with faster gemm Succeeded")
